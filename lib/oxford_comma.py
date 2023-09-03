a1ac992c318ad2ef7c119fc963efdd61edfaeb87
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-03 23:50:57 +0300

    Update oxford_comma.py

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) >= 3:
+        items[-1] = f"and {items[-1]}"
+    return ', '.join(items)
+
+# Example usage:
+items = ["apple", "banana", "cherry"]
+result = oxford_comma(items)
+print(result)
